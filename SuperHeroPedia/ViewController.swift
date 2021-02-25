6aace12932f18ba7c2a28a38320d3a9433d96e90
Américo MQ <americo.meneses.contractor@bbva.com>
2021-02-25 10:19:34 -0600

    firstCommit

---
+++ SuperHeroPedia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SuperHeroPedia
+//
+//  Created by IDS Comercial on 25/02/21.
+//  Copyright © 2021 Americo MQ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
